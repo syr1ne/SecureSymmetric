335d1bb92619919d757e910ceb81215a70e1d9ff
1byteBoy <pushpakGayen3301@gmail.com>
2024-02-24 02:54:32 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,59 @@
+## What is SecureSymmetric ?
+
+In general it's just a cool name for the project but actually it's just a simple program that encrypts file using 
+*fernet's symmetric 'secret key' based authenticated cryptography.*
+
+Learn more about **Fernet** at https://cryptography.io/en/latest/fernet/
+
+## Usage
+
+```
+$ python3 secure.py
+ 
+usage: secure.py [OPTIONS...] [PATH...]
+
+Encrypts and Decrypts plain-text data
+
+options:
+  -h, --help          show this help message and exit
+  -e, --encrypt       Encrypt the file
+  -d, --decrypt       Decrypt the file
+  -D, --dir           Work with a whole directory of files
+  -x , --extensions   Specify specific file extensions
+  -o , --output       Output file path
+  -r, --remove        Removes original file after encryting or decrypting
+
+Made with <3 by @Syrine && @1byteBoy
+```
+-----
+
+***While main options are self-explainatory but let me explain some of them a bit clearly***
+
+`-D` or `--dir` is used if we want to encrypt or decrypt all the files present in a perticular directory
+
+```
+python3 secure.py -e -D Documents/
+```
+
+`-x` or `--extensions` is used if we want to encrypt or decrypt files with specific file extension. (Works only with directory)
+
+```
+python3 secure.py -e -D Documents/Coding/ -x txt,py
+```
+
+`-o` or `--output` is used to specify a custom directory where we want to save our encrypted or decrypted files.
+
+```
+python3 secure.py -e -D Documents/Coding/ -x txt,py -o /tmp
+```
+
+`-r` or `--remove` is used to remove the files after they are either encrypted or decrptred
+
+## ToDo
+
+- [ ] Might add support for verbosity as an option
+- [ ] Support for binary files, since now it only supports plain text encryption
+
+----
+
+Made with <3 by [Syrine](https://github.com/syr1ne) and [8bitBoy](https://github.com/1byteBoy)
---
+++ secure.py
@@ -0,0 +1,180 @@
+#!/usr/bin/env python3
+import os
+import base64, hashlib
+from getpass import getpass
+from cryptography.fernet import Fernet
+from argparse import ArgumentParser, SUPPRESS
+
+# Shell colors and style, originally these are light version of the colors
+R = '\033[91m' # Light Red
+G = '\033[92m' # Light Green
+B = '\033[94m' # Light Blue
+C = '\033[96m' # Cyan
+Y = '\033[93m' # Yellow
+r = '\033[0m'  # reset color value
+
+# Success and Error color coding
+S = f'{G}*{r}'
+E = f'{R}!{r}'
+
+## set if we can write and some functions are able to perfrom
+write_file = True
+
+## generates a key with the provided password with fernets symmetric encryption algorithm 
+def gen_fernet_key (masterpass:bytes) -> bytes:
+    assert isinstance(masterpass, bytes)
+    hlib = hashlib.md5()
+    hlib.update(masterpass)
+    return base64.urlsafe_b64encode(hlib.hexdigest().encode('latin-1'))
+
+
+## takes the master password as to create the original key value 
+def input_master_key ():
+    if   args.encrypt: icon = '\U0001F512' ## unicode charecters for locked emoji
+    elif args.decrypt: icon = '\U0001F513' ## unicode charecters for unlocked emoji
+    masterpass = getpass(f"[{icon}] Enter Password: "); print() # <- OCD issue sorry
+    key = gen_fernet_key(masterpass.encode('utf-8'))
+    return Fernet(key)
+
+
+## renames encrpted and decrupted file and also stores the directory position
+def tweaking (file):
+    if file.find("/") : 
+        file_name = file.split("/")[-1]
+        ## extracts the initial file directory if any
+        file_path = '/'.join(file.split("/")[:-1]) 
+    
+    ## rename the encryped file just for the convinience of understanding
+    if   args.encrypt: file_name = "enc_" + file_name
+    ## just takes away the 'enc_' from the file name
+    ## Todo: might fix this for better logic
+    elif args.decrypt: file = file[4:]
+    return file_name, file_path
+
+
+## encryption and decryption in one function
+def operation(file, fernet):
+    dec_failed = False
+    global write_file
+    file_name, file_path  = tweaking(file)
+    with open(file, 'r') as file:
+        file.seek(0)
+        data = file.read()
+        if args.encrypt:
+            enc_data = fernet.encrypt(data.encode('utf-8'))
+        elif args.decrypt:
+            try:
+                dec_data = fernet.decrypt(data.encode('utf-8')).decode('utf-8')
+            except:
+                print(f"[{E}] Decryption Failed for {R}{file_name}{r}")
+                # as the decrytion failed, we don't delete file if '--remove' option is used 
+                dec_failed = True # <-  we might not need this cause of the next variable
+                # since the decrytion failed, we tell the write method to not write anything
+                write_file = False 
+
+    if args.decrypt: file_name = file_name[4:]
+
+    ## this is noob logic for if the process is happening in the current directory or in a diffenet path
+    if len(file_path) != 0 : file_path = file_path + '/' + file_name
+    else: file_path = file_name
+
+    if args.output : 
+        if args.output[-1] != '/':
+            file_path = args.output + '/'
+            file_path = file_path + file_name
+        else: file_path = args.output + file_name
+
+    if write_file == True:
+        with open(file_path, 'w') as output_file:
+            output_file.seek(0)
+            if args.encrypt: 
+                output_file.write(str(enc_data.decode('utf-8')))
+                print(f"[{S}] Successfully Encrypted {G}{file_name[4:]}{r}")
+            elif args.decrypt and dec_failed == False:
+                output_file.write(dec_data)
+                print(f"[{S}] Successfully Decrypted {G}{file_name}{r}")
+
+
+# checks if the single file is valid and prints related errors
+def is_valid_file (file_path):
+    try:
+        file = file_path.split("/")[-1]
+    except: pass
+    try:
+        with open(file_path, 'r') as file: return True
+    except TypeError: print(f"[{E}] Please provide a file path")
+    except FileNotFoundError: print(f"[{E}] The specified file {C}{file}{r} does not exist")
+    except PermissionError: print(f"[{E}] Permission denied for file {C}{file}{r}")
+    except IsADirectoryError: print(f"[{E}] {B}{file}{r} is a directory")
+    return False
+
+
+## some stupid directory or path sanity thingy 
+def is_valid_directory(dir_path):
+    stupid_path = -1
+    try:
+        directory = dir_path.split("/")[-1]
+        while(directory == '') : 
+            directory = dir_path.split("/")[-1 + stupid_path]
+            stupid_path = stupid_path - 1
+    except: pass
+    return directory
+
+
+# lots of flags and arguments 
+print(" ") # sorry i have OCD
+parser = ArgumentParser (description='\033[3mEncrypts and Decrypts plain-text data\033[0m', 
+                         epilog='Made with <3 by @Syrine && @1byteBoy',
+                         usage="%(prog)s [OPTIONS...] [PATH...]")
+
+parser.add_argument("-e", "--encrypt", action="store_true", help="Encrypt the file")
+parser.add_argument("-d", "--decrypt", action="store_true", help="Decrypt the file")
+parser.add_argument("-D", "--dir", action="store_true", help="Work with a whole directory of files") 
+parser.add_argument("-x", "--extensions", type=str, metavar='', help="Specify specific file extensions") 
+parser.add_argument("-o", "--output", type=str, metavar='', help="Output file path") 
+parser.add_argument("-r", "--remove", action="store_true", help="Removes original file after encryting or decrypting")
+# parser.add_argument("-v", "--verbose", action="store_true", help="For verbosity")
+parser.add_argument('path', type=str, nargs='?', help=SUPPRESS)
+args = parser.parse_args()
+
+
+# initial logic and condition sets
+if __name__ == '__main__':
+    if not any(vars(args).values()): parser.print_help()
+    elif args.encrypt and args.decrypt: print("[!] Please use only one cryptographic process")
+    elif args.encrypt or args.decrypt:
+        try:
+            if args.output : 
+                if os.path.isdir(args.output): out_dir_path = args.output
+                else : print(f"[!] Invalid Output directory {B}{is_valid_directory(args.output)}{r}"); exit(0)
+            if args.dir:
+                if args.extensions is not None:
+                    ## args.extensions will carry the list of all provided extensions ##
+                    args.extensions = [ext.strip() for ext in args.extensions.split(",")]
+                try:
+                    if os.path.isdir(args.path):
+                        dirContent = os.listdir(args.path)
+                        if len(dirContent) > 0 : 
+                            fernet = input_master_key()
+                            for content in dirContent:
+                                file = os.path.join(args.path,content)
+                                if is_valid_file(file):
+                                    if args.extensions:
+                                        for extension in args.extensions:
+                                            if file.endswith(extension):
+                                                operation(file, fernet)
+                                                break 
+                                    else: operation(file, fernet)
+                                    ## ToDo : add a confirmation prompt function 
+                                    if args.remove and write_file : os.remove(file)
+                                # else: print("Not a valid file")
+                        else: print(f"[{E}] {B}{args.path.split("/")[-1]}{r} is an empty directory")
+                    else: print(f"[{E}] {B}{is_valid_directory(args.path)}{r} is not a valid directory")
+                except TypeError as e: print(f"[{E}] Please provide a directory path ")
+            else: 
+                if is_valid_file(args.path):
+                    fernet = input_master_key()
+                    operation(args.path, fernet)
+                    if args.remove and write_file : os.remove(args.path)
+        except KeyboardInterrupt: print("\n\nBye ....")
+    else: print(f"[{E}] Please choose a cryptographic process")
