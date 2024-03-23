f8536a58fdc7f2618e851dda3ca8abfc35906342
1byteBoy <pushpakGayen3301@gmail.com>
2024-03-23 09:33:19 +0530

    v1.0.0

--- secure.py
+++ secure.py
@@ -1,155 +1,153 @@
 #!/usr/bin/env python3
+
 import os
-import base64, hashlib
+from hashlib import md5
+from platform import system
 from getpass import getpass
+from base64 import urlsafe_b64encode
 from cryptography.fernet import Fernet
 from argparse import ArgumentParser, SUPPRESS
 
-# Shell colors and style, originally these are light version of the colors
+# Shell colors, originally these are light version of the colors
 R = '\033[91m' # Light Red
 G = '\033[92m' # Light Green
 B = '\033[94m' # Light Blue
 C = '\033[96m' # Cyan
 Y = '\033[93m' # Yellow
+P = '\033[95m'
 r = '\033[0m'  # reset color value
+
+# Shell Font Style
+I = '\033[3m' # Italic
 
 # Success and Error prompt color coding
 S = f'{G}*{r}'
 E = f'{R}!{r}'
 
-## set if we can write and some functions are able to perfrom
+if   system() == 'Linux'  : separator = '/'
+elif system() == 'Windows': separator = '\\'
+
+# set if we can write and some functions are able to perfrom
 write_file = True
 
-## generates a key with the provided password with fernets symmetric encryption algorithm 
+
+# generates a key with the provided password using fernets symmetric encryption algorithm 
 def gen_fernet_key (masterpass:bytes) -> bytes:
     assert isinstance(masterpass, bytes)
-    hlib = hashlib.md5()
+    hlib = md5()
     hlib.update(masterpass)
-    return base64.urlsafe_b64encode(hlib.hexdigest().encode('latin-1'))
+    return urlsafe_b64encode(hlib.hexdigest().encode('latin-1'))
 
 
-## takes the master password as to create the original key value 
+# takes the master password as to create the original key value 
 def input_master_key ():
-    if   args.encrypt: icon = '\U0001F512' ## unicode charecters for locked emoji
-    elif args.decrypt: icon = '\U0001F513' ## unicode charecters for unlocked emoji
-    masterpass = getpass(f"[{icon}] Enter Password: "); print() # <- OCD issue sorry
+    if   args.encrypt: for_what = 'Encryption'
+    elif args.decrypt: for_what = 'Decryption'
+    masterpass = getpass(f"{P}[▬]{r} Enter {for_what} Password: "); print() # <- OCD issue sorry
     key = gen_fernet_key(masterpass.encode('utf-8'))
     return Fernet(key)
 
 
-## renames encrpted and decrupted file and also stores the directory position
-def tweaking (file):
-
-    if file.find("/") > 1 :
-        file_name = file.split("/")[-1]
-        ## extracts the initial file directory if any
-        file_path = '/'.join(file.split("/")[:-1]) 
-    else : 
-        file_name = file
-        file_path = './'
-
-    ## rename the encryped file just for the convinience of understanding
-    if  args.encrypt: file_name = "enc_" + file_name
-    ## just takes away the 'enc_' from the file name
-    ## Todo: might fix this for better logic
-    elif args.decrypt: file = file[4:]
-    return file_name, file_path
-
-
-## encryption and decryption in one function
-def enc_dec (file, fernet):
+# encryption and decryption in one function
+def process_data (file_name, path, data, fernet):
     dec_failed = False
     global write_file
-    file_name, file_path  = tweaking(file)
-    with open(file, 'r') as file:
-        file.seek(0)
-        data = file.read()
-        if args.encrypt:
-            enc_data = fernet.encrypt(data.encode('utf-8'))
-        elif args.decrypt:
-            try:
-                dec_data = fernet.decrypt(data.encode('utf-8')).decode('utf-8')
-            except:
-                print(f"[{E}] Decryption Failed for {R}{file_name}{r}")
-                # as the decrytion failed, we don't delete file if '--remove' option is used 
-                dec_failed = True # <-  we might not need this cause of the next variable
-                # since the decrytion failed, we tell the write method to not write anything
-                write_file = False 
+    if args.encrypt: enc_data = fernet.encrypt(data)
+    elif args.decrypt:
+        try:
+            dec_data = fernet.decrypt(data)
+        except:
+            print(f"[{E}] Decryption Failed for {R}{file_name}{r}")
+            # as the decrytion failed, we don't delete file if '--remove' option is used 
+            dec_failed = True 
+            # since the decrytion failed, we don't need to write anything for that file
+            write_file = False 
 
-    if args.decrypt: file_name = file_name[4:]
-
-    ## this is noob logic for if the process is happening in the current directory or in a diffenet path
-    if len(file_path) != 0 : file_path = file_path + '/' + file_name
-    else: file_path = file_name
-
-    if args.output : 
-        if args.output[-1] != '/':
-            file_path = args.output + '/'
-            file_path = file_path + file_name
-        else: file_path = args.output + file_name
+     # output file naming for both encrypted and decrypted file
+    if path != file_name :
+        if args.encrypt: file_path = path + 'enc_' + file_name
+        if args.decrypt: 
+            if 'enc_' in file_name: file_path = path + file_name[4:]
+            else: file_path = path + file_name # this will override the existing encrypted file
+    else: 
+        if args.encrypt: file_path = 'enc_' + file_name
+        if args.decrypt: 
+            if 'enc_' in file_name: file_path = file_name[4:]
+            else: file_path = file_name
 
     if write_file == True:
-        with open(file_path, 'w') as output_file:
+        with open(file_path, 'wb') as output_file:
             output_file.seek(0)
             if args.encrypt: 
-                output_file.write(str(enc_data.decode('utf-8')))
-                print(f"[{S}] Successfully Encrypted {G}{file_name[4:]}{r}")
+                output_file.write(enc_data)
+                print(f"[{S}] Successfully Encrypted {G}{file_name}{r}")
             elif args.decrypt and dec_failed == False:
                 output_file.write(dec_data)
                 print(f"[{S}] Successfully Decrypted {G}{file_name}{r}")
 
 
+# i have no idea why i have to work so hard on this, but i do
+def path_handling (path):
+    bogus_path = 1 
+    try:
+        if path.find(separator) >= 0:
+            obj_name = path.split(separator)[-1]
+            while obj_name == '': 
+                obj_name = path.split(separator)[-1 - bogus_path]
+                bogus_path = bogus_path + 1
+        else: obj_name = path
+    except AttributeError:
+        if args.dir: print(f"[{E}] Please provide a directory path")
+        else: print(f"[{E}] Please provide a file path")
+        exit()
+
+    obj_path = separator.join(path.split(separator)[:-bogus_path])
+    if len(obj_path) != 0: obj_path = obj_path + separator
+    else: obj_path = obj_name
+    return obj_name, obj_path
+
+
 # checks if the single file is valid and prints related errors
-def is_valid_file (file_path):
-    stupid_path = -1 
+def process_file (file_path):
+    fileName, path = path_handling(file_path)
+    if fileName != path: filePath = path + fileName
+    else: filePath = fileName
     try:
-        if file_path.find("/") > 1:
-            file = file_path.split("/")[-1]
-            while(file == '') : 
-                file = file_path.split("/")[-1 + stupid_path]
-                stupid_path = stupid_path - 1
-        else: file = file_path
-    except AttributeError: 
-        print(f"[{E}] Please provide a file path"); exit(0)
-
-    try:
-        with open(file_path, 'r') as theFile:
-            _ = theFile.read()
-            return True
-    except FileNotFoundError : print(f"[{E}] The specified file {C}{file}{r} does not exist")
-    except PermissionError   : print(f"[{E}] Permission denied for file {C}{file}{r}")
-    except IsADirectoryError : print(f"[{E}] {B}{file}{r} is a directory")
-    except UnicodeDecodeError: print(f"[{E}] Sorry, binary file ({G}{file}{r}) support coming soon!")
-    return False
+        with open(filePath, 'rb') as theFile:
+            theFile.seek(0)
+            data = theFile.read() 
+            return True, fileName, path, data
+    except FileNotFoundError : print(f"[{E}] The specified file {C}{fileName}{r} does not exist")
+    except PermissionError   : print(f"[{E}] Permission denied for file {C}{fileName}{r}")
+    except IsADirectoryError : print(f"[{E}] {B}{fileName}{r} is a directory")
+    except NotADirectoryError: print(f"[{E}] What the $%&* is {Y}{fileName}{r}")
+    except UnicodeDecodeError: print(f"[{E}] Sorry, binary file ({G}{fileName}{r}) support coming soon!")
+    return False, None, None, None
 
 
-## Fancy Directory check errors 
-def is_valid_directory (dir_path):
-    stupid_path = -1 # to check from the negetive index 
-    try:
-        if dir_path.find("/") > 1:
-            directory = dir_path.split("/")[-1]
-            while(directory == '') : 
-                directory = dir_path.split("/")[-1 + stupid_path]
-                stupid_path = stupid_path - 1
-    except AttributeError : 
-        print(f"[{E}] Please provide a directory path")
-        return False
+# directories baby 
+def is_valid_directory (dir_path, is_out_dir=None):
+    directory_name, path = path_handling(dir_path)
+    if is_out_dir:
+        if directory_name == path: path = path + separator 
+        else: path = path + directory_name + separator
     try:
         dirContent = os.listdir(dir_path)
-        if len(dirContent) > 0 : return True
-        else: print(f"[{E}] {B}{is_valid_directory(args.path)}{r} is an empty directory")
+        if len(dirContent) > 0 or is_out_dir:
+            if is_out_dir: return True, path
+            else: return True
+        else: print(f"[{E}] {B}{directory_name}{r} is an empty directory"); exit()
     except FileNotFoundError: 
-        if args.output: print(f"[{E}] Invalid output directory {B}{directory}{r} "); exit(0)
-        else: print(f"[{E}] The specified directory {B}{directory}{r} does not exist")
-    except PermissionError   : print(f"[{E}] Permission denied for folder {B}{directory}{r}")
-    except NotADirectoryError: print(f"[{E}] {B}{directory}{r} is not a directory")
+        if is_out_dir: print(f"[{E}] Invalid output directory {B}{directory_name}{r} "); exit()
+        else: print(f"[{E}] The specified directory {B}{directory_name}{r} does not exist")
+    except PermissionError   : print(f"[{E}] Permission denied for folder {B}{directory_name}{r}")
+    except NotADirectoryError: print(f"[{E}] {B}{directory_name}{r} is not a directory")
     return False
     
 
 # lots of flags and arguments 
 print(" ") # sorry i have OCD
-parser = ArgumentParser (description='\033[3mEncrypts and Decrypts plain-text data\033[0m', 
+parser = ArgumentParser (description=f'{I}Encrypts and Decrypts plain-text data{r}', 
                          epilog='Made with <3 by @Syrine && @1byteBoy',
                          usage="%(prog)s [OPTIONS...] [PATH...]")
 
@@ -159,41 +157,45 @@ parser.add_argument("-D", "--dir", action="store_true", help="Work with a whole 
 parser.add_argument("-x", "--extensions", type=str, metavar='', help="Specify specific file extensions") 
 parser.add_argument("-o", "--output", type=str, metavar='', help="Output file path") 
 parser.add_argument("-r", "--remove", action="store_true", help="Removes original file after encryting or decrypting")
-# parser.add_argument("-v", "--verbose", action="store_true", help="For verbosity")
+parser.add_argument("-v", "--verbose", action="store_true", help="For verbosity")
 parser.add_argument('path', type=str, nargs='?', help=SUPPRESS)
 args = parser.parse_args()
 
 
 # initial logic and condition sets
 if __name__ == '__main__':
+    its_out_dir = False
     if not any(vars(args).values()): parser.print_help()
-    elif args.encrypt and args.decrypt: print(f"[{R}!{r}] Please use only one cryptographic process")
+    elif args.encrypt and args.decrypt: 
+        print(f"[{R}!{r}] Please use only one cryptographic process")
     elif args.encrypt or args.decrypt:
         try:
             if args.output : 
-                if is_valid_directory(args.output): out_dir_path = args.output
-                # else : print(f"[!] Invalid Output directory {B}{is_valid_directory(args.output)}{r}"); exit(0)
+                its_out_dir, out_dir_path = is_valid_directory(args.output, True)
             if args.dir:
                 if args.extensions is not None:
-                    ## args.extensions will carry the list of all provided extensions ##
                     args.extensions = [ext.strip() for ext in args.extensions.split(",")]
                 if is_valid_directory(args.path):
-                    dirContent = os.listdir(args.path)
                     fernet = input_master_key()
+                    dirContent = os.listdir(args.path)
                     for content in dirContent:
-                        file = os.path.join(args.path,content)
-                        if is_valid_file(file):
+                        file_path = os.path.join(args.path, content)
+                        is_file, file_name, path, data = process_file(file_path)
+                        if its_out_dir : path = out_dir_path
+                        if is_file:
                             if args.extensions:
                                 for extension in args.extensions:
-                                    if file.endswith(extension):
-                                        enc_dec(file, fernet)
+                                    if file_path.endswith(extension):
+                                        process_data(file_name, path, data, fernet)
                                         break 
-                            else: enc_dec(file, fernet)
-                            if args.remove and write_file : os.remove(file)
-            else: 
-                if is_valid_file(args.path):
+                            else: process_data(file_name, path, data, fernet)
+                            if args.remove and write_file : os.remove(file_path)
+            else:
+                is_file, file_name, path, data = process_file(args.path)
+                if its_out_dir : path = out_dir_path
+                if is_file:
                     fernet = input_master_key()
-                    enc_dec(args.path, fernet)
+                    process_data(file_name, path, data, fernet)
                     if args.remove and write_file : os.remove(args.path)
-        except KeyboardInterrupt: print("\n\nBye ....")
+        except KeyboardInterrupt: print(f"\n\n[»] Bye")
     else: print(f"[{E}] Please choose a cryptographic process")
